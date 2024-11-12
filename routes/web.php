2b4404cc8a9bb355a3eaee850ad35416d2bdf122
David Dwi Harianto <dapitharianto1912@gmail.com>
2024-11-12 20:44:44 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,27 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+<<<<<<< Updated upstream
+=======
+
+Route::controller( LoginController::class)->group(function(){
+    Route::get('/login', 'index')->name('login.index');
+    Route::post('/login/verify', 'verify')->name('login.verify');
+});
+
+Route::controller(AdminController::class)->group(function(){
+    Route::get('/admin', 'index')->name('admin.index');
+    Route::get('/admin/logout', 'logout')->name('admin.logout');
+});
+
+Route::controller(OrderController::class)->group(function() { 
+    Route::get('/order/table/{tableNumber}', 'showTableForm');
+    Route::post('/order/table/{tableNumber}', 'storeCustomer');
+    Route::get('/order/{tableNumber}/menu', 'showMenu');
+    Route::post('/order/{tableNumber}/menu', 'checkout');
+});
+
+Route::get('/input', function () {
+    return view('/customer/input');
+});
+>>>>>>> Stashed changes
