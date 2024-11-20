019a1e585cac46d0a2424b39a50fef72a171fa4e
David Dwi Harianto <dapitharianto1912@gmail.com>
2024-11-20 13:30:33 +0700

    update icon

--- resources/views/customer_ex/home.blade.php
+++ resources/views/customer_ex/home.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="{{ asset('storage/asset/gambar/icon.png') }}" type="image/x-icon">
     @vite('resources/css/app.css') 
     <style>
         .slide {
--- resources/views/customer_ex/input.blade.php
+++ resources/views/customer_ex/input.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Koffnes</title>
+    <link rel="icon" href="{{ asset('storage/asset/gambar/icon.png') }}" type="image/x-icon">
     @vite('resources/css/app.css')
     <style>
       body {
