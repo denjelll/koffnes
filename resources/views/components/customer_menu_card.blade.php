f6cfdb117bc1105b4cfcc475a74255f1a4b87dcd
gratefulboi <134521848+gratefulboi@users.noreply.github.com>
2024-11-30 13:55:08 +0700

    balikin component bbrp

---
+++ resources/views/components/customer_menu_card.blade.php
@@ -0,0 +1,34 @@
+<div
+          class="w-1/2 rounded-lg shadow-lg overflow-hidden mt-5"
+          style="background-color: #fff2e2; box-shadow: 8px 10px 0px -2px rgba(106,111,76,0.72);
+-webkit-box-shadow: 8px 10px 0px -2px rgba(106,111,76,0.72);
+-moz-box-shadow: 8px 10px 0px -2px rgba(106,111,76,0.72);"
+        >
+          <img
+            src="foto-menu.jpg"
+            alt="Foto Menu"
+            class="w-full h-48 object-cover"
+          />
+          <div class="p-4">
+            <h3 class="text-xl font-semibold text-gray-800">Nama Menu</h3>
+            <p class="text-lg text-green-600">Rp 25.000</p>
+            <p class="text-sm text-gray-600 mt-2">
+              Deskripsi singkat mengenai menu ini.
+            </p>
+            <!-- <button
+              class="mt-4 w-full py-2 text-white rounded-lg"
+              style="background-color: #412f26"
+            >
+              Add to Cart
+            </button> -->
+            <div class="flex items-center mt-4 ml-4">
+                <button onclick="" class="bg-cocoa w-8 h-8 flex items-center justify-center rounded-full mr-2" style="color:white;">
+                    -
+                </button>
+                <span id="americanoQuantity" class="mx-4 text-gray-800 font-semibold">1</span>
+                <button onclick="" class="bg-cocoa w-8 h-8 flex items-center justify-center rounded-full ml-2" style="color:white;">
+                    +
+                </button>
+            </div>
+          </div>
+</div>
