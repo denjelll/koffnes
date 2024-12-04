330724b004ee0d75394ab23acbbe6f51744dcdd8
David Dwi Harianto <dapitharianto1912@gmail.com>
2024-12-04 21:28:53 +0700

    update closed

--- resources/views/closed.blade.php
+++ resources/views/closed.blade.php
@@ -5,9 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     <title>Closed</title>
+    <style>
+        html {
+            overflow-y: scroll;
+            scrollbar-width: none; /* Untuk Firefox */
+        }
+
+        html::-webkit-scrollbar {
+            display: none; /* Untuk Chrome, Safari, dan Edge */
+        }
+
+        body {
+      background-image: url("{{ asset('storage/asset/gambar/motif.png') }}");
+      background-repeat: repeat;
+      background-position: top left;
+      background-size: 400px 400px;
+      margin: 0;
+        }
+
+    </style>
 </head>
-<body>
-    <h1>Sorry, We Are Closed!</h1>
-    <p>Our system is currently not accepting orders. Please come back later!</p>
+<body class="flex flex-col min-h-screen items-center justify-center text-center">
+    <x-customer_header />
+    <div class="flex flex-col items-center">
+        <img src="{{ asset('storage/asset/gambar/close.gif') }}" alt="Animated GIF" class="mb-4" style="width: 60%; max-width: 300px;">
+        <h1 class="font-semibold" style="font-size: 24px">Yahh Masih Tutup 🥲<br>Tunggu buka yah</h1>
+    </div>
+    <!-- Fixed Bottom Navbar -->
 </body>
 </html>
